8111c67d89aff0937eb3328a8a9eaec94afa456d
David Hirvonen <dhirvonen@elucideye.com>
2017-11-19 10:42:48 -0500

    add local config for local keep package sources test

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,3 @@
+#hunter_config(GTest GIT_SUBMODULE src/3rdparty/gtest)
+#hunter_config(GTest GIT_SUBMODULE src/3rdparty/gtest KEEP_PACKAGE_SOURCES)
+hunter_config(GTest VERSION ${HUNTER_GTest_VERSION} KEEP_PACKAGE_SOURCES)
